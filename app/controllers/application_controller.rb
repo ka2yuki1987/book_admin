738512cae59ef97cfe7835d19d64315d6ef69495
ka2yuki1987 <213430954+ka2yuki1987@users.noreply.github.com>
2025-05-29 18:48:35 +0900

    コントローラとビューの協調とビューテンプレートの基本

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,11 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
-  allow_browser versions: :modern
+  # allow_browser versions: :modern
+  before_action :detect_mobile_variant
+
+  private
+  
+  def detect_mobile_variant
+    request.variant = :mobile if request.user_agent =~ /iPhone/
+  end
 end
